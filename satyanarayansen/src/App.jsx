5b9436d594b7db449700e35454b2e9f7183b582c
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2024-01-12 19:46:58 +0530

    navbar added with fully responsive

--- satyanarayansen/src/App.jsx
+++ satyanarayansen/src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import LinksNavbar from './components/LinksNavbar';
 
 function App() {
 
   return (
     <>
-      
+      <LinksNavbar />
+      <Navbar/>
+      <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>}/>
+          </Route>
+      </Routes>
     </>
   )
 }
---
+++ satyanarayansen/src/components/LinksNavbar.jsx
@@ -0,0 +1,29 @@
+import { MdOutlineMail } from "react-icons/md";
+import { FaFacebookF } from "react-icons/fa";
+import { TiSocialDribbble } from "react-icons/ti";
+import { TiSocialLinkedin } from "react-icons/ti";
+import { AiOutlineBehance } from "react-icons/ai";
+import { MdOutlineCall } from "react-icons/md";
+const LinksNavbar = () => {
+    return (
+        <div className="sm:flex sm:flex-col md:flex md:flex-row lg:flex lg:flex-row items-center justify-between    px-10 py-3 bg-gray-800 text-white border-b-[0.5px] border-gray-300 fixed top-0 z-50 w-full">
+            <div className="flex items-center justify-center gap-3">
+                <div><MdOutlineMail /></div>
+                <h1 className="">satyanarayan74sen@gmail.com</h1>
+            </div>
+            <div className="flex items-center justify-center gap-6">
+                <div><FaFacebookF /></div>
+                <div><TiSocialDribbble /></div>
+                <div><TiSocialLinkedin /></div>
+                <div><AiOutlineBehance /></div>
+                <div className="h-[2rem]  w-[1px] bg-white"></div>
+                <div className="flex items-center gap-3">
+                    <div><MdOutlineCall /></div>
+                    <a href="" className="">7908226306</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LinksNavbar;

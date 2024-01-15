818c38b91c482d5c88873a9ffc29c19a062e5250
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2024-01-15 11:28:31 +0530

    updated

--- satyanarayansen/src/components/HeroPage.jsx
+++ satyanarayansen/src/components/HeroPage.jsx
@@ -3,7 +3,7 @@ const HeroPage = () => {
     return (
         <div className="w-full flex justify-between items-center ">
             <div className="w-[50%] flex flex-col items-start lg:ml-24 ">
-                <h1 className="text-4xl font-bold">Hi, I'm Satyanarayan sen</h1>
+                <h1 className="text-4xl font-bold">Hi, I am Satyanarayan sen</h1>
                 <p>I am a MERN stack Developer</p>
                 <p>Building Websites to solve real life problems</p>
                 <button>

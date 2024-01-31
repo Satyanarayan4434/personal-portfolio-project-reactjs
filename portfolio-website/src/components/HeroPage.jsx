d6d4ae07c908e38b73b7539e1faccfbde3acfb91
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2024-02-01 01:12:13 +0530

    hero page updated

--- portfolio-website/src/components/HeroPage.jsx
+++ portfolio-website/src/components/HeroPage.jsx
@@ -1,23 +1,29 @@
 import heroImg from "../assets/heroIMG-1.png"
+import { BsSendArrowDown } from "react-icons/bs";
+import { CiDesktopMouse2 } from "react-icons/ci";
+import { IoIosArrowRoundDown } from "react-icons/io";
 const HeroPage = () => {
     return (
-        <div className="w-full flex justify-between items-center bg-[#424769] overflow-hidden relative">
+        <div className="w-full flex justify-between items-center bg-[#B2C8BA] overflow-hidden relative">
             <div className="w-[50%] flex flex-col gap-5 items-start lg:ml-24 ">
                 <h1 className="text-4xl font-bold">Hi, I am Satyanarayan sen</h1>
-                <p className="text-2xl font-semibold ">I am a MERN stack Developer</p>
-                <p className="">Building Websites to solve real life problems</p>
-                <button>
+                <p className="text-xl font-semibold opacity-85">I am a MERN stack Developer</p>
+                <p className="text-xl">Building Websites to solve real life problems</p>
+                <button className="bg-[#1640D6] text-white px-4 py-3 rounded-md flex gap-3">
                     <p>Contact Me</p>
+                    <BsSendArrowDown  size={"1.5em"} className="rotate-45"/>
                 </button>
-                <div>
+                <div className="flex items-center gap-3 text-xl text-[#1640D6] font-semibold">
+                    <CiDesktopMouse2 />
                     <p>Scroll Down</p>
+                    <IoIosArrowRoundDown className="animate-bounce"/>
                 </div>
             </div>
             <div className="z-10">
-                <img src={heroImg} alt="" className="w-[37rem] brightness-90"/>
+                <img src={heroImg} alt="" className="w-[44rem] brightness-90"/>
                 
             </div>
-            <div className="w-[70rem] -z-0 h-[38.7rem] bg-[#E5E1DA]  rounded-t-full absolute -top-10 -right-[22rem]"></div>
+            <div className="w-[70rem] -z-0 h-[44rem] bg-slate-700 shadow-2xl shadow-black rounded-t-[50rem] absolute -top-10 -right-[22rem]"></div>
         </div>
     );
 }

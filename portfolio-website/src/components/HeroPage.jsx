4420fcf0e121f476f0a7ca85e912ccb978a08450
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2024-02-06 11:10:08 +0530

    hero page responsive

--- portfolio-website/src/components/HeroPage.jsx
+++ portfolio-website/src/components/HeroPage.jsx
@@ -1,15 +1,16 @@
 import heroImg from "../assets/heroIMG-1.png";
+import heroImg2 from "../assets/heroIMG.png";
 import qutosImg from "../assets/qutos.png"
 import { BsSendArrowDown } from "react-icons/bs";
 import { CiDesktopMouse2 } from "react-icons/ci";
 import { IoIosArrowRoundDown } from "react-icons/io";
 const HeroPage = () => {
     return (
-        <div className="w-full flex justify-between items-center bg-[#B2C8BA] overflow-hidden relative">
-            <div className="w-[50%] flex flex-col gap-5 items-start lg:ml-24 ">
-                <h1 className="text-[4rem] z-10 font-bold "><span className="font-extrabold text-blue-800">Hi,</span> I am Satyanarayan sen</h1>
-                <p className="text-xl font-semibold opacity-85 text-blue-800">I am a MERN stack Developer</p>
-                <p className="text-xl text-blue-900">Building Websites to solve real life problems</p>
+        <div className="w-full sm:flex sm:flex-col  md:flex md:flex-row lg:flex lg:flex-row lg:justify-between gap-28  lg:items-center bg-[#B2C8BA] overflow-hidden relative pt-12 lg:pt-0">
+            <div className="w-[20rem] mx-auto lg:w-[50%] flex flex-col gap-2 lg:gap-5  lg:items-start justify-center items-center  lg:ml-24 ">
+                <h1 className="text-[1.5rem] md:text-[3rem] lg:text-[4rem]  z-10 font-bold "><span className="font-extrabold text-blue-800">Hi,</span> I am Satyanarayan sen</h1>
+                <p className="text-[1.2rem] font-semibold opacity-85 text-blue-800">I am a MERN stack Developer</p>
+                <p className="text-[1rem] lg:text-xl text-blue-900">Building Websites to solve real life problems</p>
                 <button className="bg-[#1640D6] text-white px-4 py-3 rounded-md flex gap-3">
                     <p>Contact Me</p>
                     <BsSendArrowDown  size={"1.5em"} className="rotate-45"/>
@@ -20,12 +21,16 @@ const HeroPage = () => {
                     <IoIosArrowRoundDown className="animate-bounce"/>
                 </div>
             </div>
-            <div className="z-10">
-                <img src={heroImg} alt="" className="w-[44rem] brightness-90"/>                
+            <div className="z-10 hidden lg:flex">
+                <img src={heroImg} alt="" className="lg:w-[44rem] lg:brightness-90"/>                
+            
             </div>
-            <div className="w-[70rem] -z-0 h-[44rem] bg-slate-700 shadow-2xl shadow-black rounded-t-[50rem] absolute -top-10 -right-[22rem]"></div>
-            <img src={qutosImg} alt="" className="absolute top-[14rem] left-[39.3rem] opacity-35 -z-0 w-[3rem]"/>
-            <img src={qutosImg} alt="" className="absolute top-[7rem] left-[2.5rem] opacity-35 -z-0 w-[3rem] rotate-180"/>
+            <div className="z-10 lg:hidden sm:flex py-14">                      
+                <img src={heroImg2} alt="" className="lg:w-[44rem] w-[20rem] mx-auto"/> 
+            </div>
+            <div className="lg:w-[70rem] -z-0 lg:h-[44rem] bg-slate-700 shadow-2xl shadow-black lg:rounded-t-[50rem] absolute lg:-top-10 lg:-right-[22rem]"></div>
+            <img src={qutosImg} alt="" className="absolute lg:top-[14rem] lg:left-[39.3rem] opacity-35 -z-0 sm:hidden md:w-[3rem] lg:w-[3rem]"/>
+            <img src={qutosImg} alt="" className="absolute lg:top-[7rem] lg:left-[2.5rem] opacity-35 -z-0 sm:hidden  md:w-[3rem] lg:w-[3rem]  rotate-180"/>
         </div>
     );
 }
